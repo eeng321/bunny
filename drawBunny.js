fb091e563f5cd59e9fb99434a2d37038b4d628ec
edmond <edmond.e321@gmail.com>
2019-03-19 23:20:55 -0700

    fix normals

--- drawBunny.js
+++ drawBunny.js
@@ -4,7 +4,7 @@ var program;
 var height = 0.0;
 var vBuffer;
 var normalBuffer;
-var TRANSLATE_SPEED = 800;
+var TRANSLATE_SPEED = 600;
 
 //transformations
 var scale;
@@ -106,7 +106,6 @@ function render() {
 
     gl.enable(gl.DEPTH_TEST);
     gl.enable(gl.CULL_FACE);
-
     
     setColor();
     transformation();
@@ -121,7 +120,7 @@ function reset(){
 }
 
 function setColor(){
-    var c = [0.8, 0.898039, 0.196078];
+    var c = [0.8, 0.598039, 0.196078];
     var color = gl.getUniformLocation(program, "color");
     gl.uniform3fv(color, c);
 }
@@ -137,15 +136,13 @@ function getBunny(){
 
 function getNormals(){
     var t1, t2;
-    for (var i = 2; i < bunny.length; i++) {
-        t1 = subtract(bunny[i-2], bunny[i-1]);
-        t2 = subtract(bunny[i-2], bunny[i]);
-        normals.push(cross(t1, t2));
+    for (var i = 2; i < bunny.length; i = i+3) {
+        t1 = subtract(bunny[i-1], bunny[i-2]);
+        t2 = subtract(bunny[i], bunny[i-2]);
+        for(var j = 0; j < 3; j++){
+            normals.push(normalize(cross(t1, t2)));
+        }
     }
-    // missing 2 elements since i started at 2
-    normals.push(cross(t1, t2));
-    normals.push(cross(t1, t2));
-    //console.log(bunny.length + "/" + normals.length);
 }
 
 function fillBuffers(){
